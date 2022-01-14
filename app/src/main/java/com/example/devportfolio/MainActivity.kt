0c119c4af40ad8b3ca706e6cb64875a838d12f21
prasannakumar2495 <prasannakumar2461995@gmail.com>
2022-01-14 14:46:23 +0530

    added view binding

--- app/src/main/java/com/example/devportfolio/MainActivity.kt
+++ app/src/main/java/com/example/devportfolio/MainActivity.kt
@@ -4,16 +4,24 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.devportfolio.controller.DevPagerAdapter
+import com.example.devportfolio.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //this will be supported only for ViewPager not for ViewPager2
         viewPager.adapter = DevPagerAdapter(supportFragmentManager)
-        tabLayoutID.setupWithViewPager(viewPager)
-        tabLayoutID.setTabTextColors(Color.BLACK, Color.WHITE)
+        GlobalScope.launch {
+            tabLayoutID.setupWithViewPager(viewPager)
+            tabLayoutID.setTabTextColors(Color.BLACK, Color.WHITE)
+        }
+
     }
 }
